2f8697d197605e5968708ee8e78ff309953facb4
denisromanov601-coder <denisromanov601@gmail.com>
2026-04-23 10:58:33 +0300

    Made interface UOW for session

---
+++ src/application/interfaces/unit_of_work.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from abc import ABC , abstractmethod
+
+from application.interfaces.repositories.users import IUserRepository
+
+
+class IUnitOFWork(ABC):
+    users:IUserRepository
+
+    async def __aenter__(self):
+        return self
+    
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        if exc_type:
+            await self.rollback()
+        else:
+            await self.commit()
+
+    @abstractmethod
+    async def commit(self) -> None: ...
+    @abstractmethod
+    async def rollback(self) -> None: ...
